44b84b107c58af9706da719f4f42d49f980aa7f1
Léo Davesne <leo@davesne.net>
2017-07-07 10:29:27 +0200

    Post a message in Slack

---
+++ slack_webhook_post.py
@@ -0,0 +1,23 @@
+'''
+This is an example of how to send data to Slack webhooks in Python with the
+requests module.
+Detailed documentation of Slack Incoming Webhooks:
+https://api.slack.com/incoming-webhooks
+'''
+
+import json
+import requests
+
+# Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
+webhook_url = 'https://hooks.slack.com/services/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'
+slack_data = {'text': "Sup! We're hacking shit together @HackSussex :spaghetti:"}
+
+response = requests.post(
+    webhook_url, data=json.dumps(slack_data),
+    headers={'Content-Type': 'application/json'}
+)
+if response.status_code != 200:
+    raise ValueError(
+        'Request to slack returned an error %s, the response is:\n%s'
+        % (response.status_code, response.text)
+    )
